4a106ed590ab5da6009711fb29655cd425b04e05
MaximoGismondi <maximogismondi@gmail.com>
2024-09-19 19:18:43 -0300

    misstypo

--- parcialitos/parcialito2/110119_MaximoGismondi_5.sql
+++ parcialitos/parcialito2/110119_MaximoGismondi_5.sql
@@ -1,3 +1,4 @@
+-- Active: 1726096306815@@localhost@5432@parcialito_sql
 select a.padron, n.codigo as codigo_departamento, n.numero, n.nota
 from alumnos a
     join notas n on a.padron = n.padron
@@ -9,6 +10,6 @@ where
 -- Resultados --
 -- padron,codigo_departamento,numero,nota
 -- 88000,75,1,9
--- 88000,72,14,8
+-- 88000,71,14,8
 -- 88000,75,42,7
 -- 88000,75,6,9
