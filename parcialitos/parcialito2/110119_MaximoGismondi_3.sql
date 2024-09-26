2059b07ff6d26b10c2bb231a6f4637995200854b
MaximoGismondi <maximogismondi@gmail.com>
2024-09-25 23:26:53 -0300

    Mejora en ejercicio 3

--- parcialitos/parcialito2/110119_MaximoGismondi_3.sql
+++ parcialitos/parcialito2/110119_MaximoGismondi_3.sql
@@ -1,19 +1,13 @@
 select
-    c.codigo as codigo_carrera,
-    d.codigo as codigo_departamento,
+    i.codigo as codigo_carrera,
+    n.codigo as codigo_departamento,
     round(avg(n.nota), 2) as promedio -- Redondeo a 2 decimales
-from
-    carreras c
-    join inscripto_en i on c.codigo = i.codigo
-    join alumnos a on i.padron = a.padron
-    join notas n on a.padron = n.padron
-    join materias m on n.codigo = m.codigo
-    and n.numero = m.numero
-    join departamentos d on m.codigo = d.codigo
+from inscripto_en i
+    join notas n on i.padron = n.padron
 group by
-    c.codigo,
-    d.codigo
-order by c.codigo, d.codigo;
+    i.codigo,
+    n.codigo
+order by i.codigo, n.codigo;
 -- Resultados --
 -- codigo_carrera,codigo_departamento,promedio
 -- 5,61,7.00
